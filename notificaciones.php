d5ab49849c2e4012a32e8d0686c90900a129e6f7
ElFoco <luis008m@hotmail.com>
2021-05-02 22:14:44 -0600

    pruebas base de datos

--- notificaciones.php
+++ notificaciones.php
@@ -24,7 +24,7 @@
         switch($_POST["type"]) {
             case "payment":
                 $datos = json_encode($_POST["data"]);
-                $sqlquery = "INSERT INTO registros (datos) VALUES ('$datos');";
+                $sqlquery = "INSERT INTO registros (registro) VALUES ('$datos');";
                 $result = pg_query($pg_conn, $sqlquery);
                 break;  
             case "plan":
